f13d4afafe864e54ded62c08f8073c07fb12beeb
gilianfavour <gilianfavour06gmail.com>
2026-05-28 10:27:51 +0300

    add product providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'providers/product_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  runApp(
+
+    MultiProvider(
+
+      providers: [
+
+        ChangeNotifierProvider(
+
+          create: (_) => ProductProvider(),
+        ),
+      ],
+
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
